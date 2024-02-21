074dce50be9920047a21fe80b06fa994e383523a
ahmet-eker <141547404+ahmet-eker@users.noreply.github.com>
2024-02-21 11:29:06 +0300

    Delete exercises directory

--- exercises/exercise_5/Exercise1.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter a nummber: "))
-
-
-my_dict = {i: "*" * i for i in range(1,number+1)}
-
-for a in range(1,number+1):
-    print(my_dict[a])
--- exercises/exercise_6/Exercise1.py
+++
@@ -1,6 +0,0 @@
-s = int(input("Enter an integer: "))
-
-
-my_dict = {i: "*" * i for i in range(1,s+1)}
-for k in my_dict.values():
-    print(k)
